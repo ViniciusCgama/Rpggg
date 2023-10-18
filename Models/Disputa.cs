51ab2f0b96c9b3a456792c85085418e7f469f2f2
viniciusCgama <viniciuscgama@hotmail.com>
2023-10-17 22:33:13 -0300

    First commit

---
+++ Models/Disputa.cs
@@ -0,0 +1,34 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RpgApi.Models
+{
+    [Table("TB_DISPUTAS")]
+    public class Disputa
+    {
+        [Key]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Column("Dt_Disputa")]
+        public DateTime? DataDisputa { get; set; }
+
+        [Column("AtacanteId")]
+        public int AtacanteId { get; set; }
+
+        [Column("OponenteId")]
+        public int OponenteId { get; set; }
+        
+        [Column("Tx_Narracao")]
+        public string Narracao { get; set; } = string.Empty;
+
+        [NotMapped]
+        public int? HabilidadeId { get; set; }
+
+        [NotMapped]
+        public List<int>? ListaIdPersonagens { get; set; }
+        
+        [NotMapped]
+        public List<string>? Resultados { get; set; }
+    }
+}
